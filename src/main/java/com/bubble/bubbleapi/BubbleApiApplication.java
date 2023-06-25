cec6061121ddcd00a7a905c0134b3220557bfa8e
Nikhil Nayak <nikhil.nixel@gmail.com>
2023-06-25 14:41:25 +0530

    springboot starter code

---
+++ src/main/java/com/bubble/bubbleapi/BubbleApiApplication.java
@@ -0,0 +1,13 @@
+package com.bubble.bubbleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BubbleApiApplication.class, args);
+    }
+
+}
